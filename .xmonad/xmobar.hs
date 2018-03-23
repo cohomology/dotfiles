6d6244f983aa4efdbca7cf77eaae1104046403fe
Kilian Kilger <kkilger@gmail.com>
2018-03-23 07:41:55 +0100

    Make some adjustments

--- .xmonad/xmobar.hs
+++ .xmonad/xmobar.hs
@@ -8,16 +8,18 @@ Config { font = "-*-Fixed-Bold-R-Normal-*-13-*-*-*-*-*-*-*"
         , hideOnStart = False
         , persistent = True
         , position = TopW L 100
-        , commands = [  Run Network "eno1" ["-L","0","-H","32","--normal","green","--high","red"] 10
+        , commands = [  Run Network "wlp1s0" ["-L","0","-H","32","--normal","green","--high","red"] 10
                         , Run Cpu ["-L","3","-H","50","--normal","green","--high","red"] 10
                         , Run Memory ["-t","Mem: <usedratio>%"] 10
                         , Run Swap [] 10
                         , Run Com "uname" ["-s","-r"] "" 36000
                         , Run Date "%a %b %_d %Y %H:%M:%S" "date" 10
                         , Run Com "/home/kilian/.xmonad/bin/show-volume.py" [] "vol" 1
+                        , Run BatteryP ["BAT0"] [ "-t", "Bat: <left>%", "-L", "50", "-H", 
+                                                  "75", "-l", "red", "-n", "yellow", "-h", "green"] 10
                         , Run StdinReader
                         ]
         , sepChar = "%"
         , alignSep = "}{"
-        , template = "%StdinReader% | %cpu% | %memory% * %swap% | %eno1% }{ %vol% | %uname% | <fc=#ee9a00>%date%</fc>"
+        , template = "%StdinReader% | %cpu% | %memory% * %swap% | %wlp1s0% | %battery% }{ %vol% | %uname% | <fc=#ee9a00>%date%</fc>"
         }
